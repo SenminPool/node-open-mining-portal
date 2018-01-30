76be9b36652e9d87a9af178edf67d4bca849445f
k725 <k725@users.noreply.github.com>
2018-01-30 21:39:19 +0900

    Backport from server

--- libs/website.js
+++ libs/website.js
@@ -95,7 +95,7 @@ module.exports = function(logger){
 
 
     //If an html file was changed reload it
-    watch('website', function(filename){
+    watch('website', { recursive: true }, function(evt, filename){
         var basename = path.basename(filename);
         if (basename in pageFiles){
             console.log(filename);
